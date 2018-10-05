1ce4cb0be13d2566015bd2bdb641309bc435a85f
alencar.lidiane@gmail.com <Alencar14>
2018-10-04 22:48:51 -0300

    finish him

--- app/src/main/java/com/example/logonrmlocal/servicesebroadcastreceiver/AlarmeReceiver.kt
+++ app/src/main/java/com/example/logonrmlocal/servicesebroadcastreceiver/AlarmeReceiver.kt
@@ -1,2 +1,16 @@
 package com.example.logonrmlocal.servicesebroadcastreceiver
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
+import android.widget.Toast
+
+class AlarmeReceiver : BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent?) {
+        MediaPlayer.create(context, R.raw.alarm).start()
+        Toast.makeText(context,
+                "Alarme",
+                Toast.LENGTH_SHORT).show()
+    }
+}
